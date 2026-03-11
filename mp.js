56a79c97669408323e342ef8366a4f459a19f2f7
RAYYAN-PROGRAMMAR <emprolst.onion@gmail.com>
2026-03-12 00:30:29 +0600

    Add files via upload

---
+++ mp.js
@@ -0,0 +1,98 @@
+console.log('hello world');
+var carsname = "Bmw, Lamborghini, Rolls Royalce";
+
+console.log(carsname)
+
+var strType = "Rayyan-type";
+
+console.log( typeof strType)
+
+var numType = 30;
+
+console.log(numType)
+
+var BoolType = false;
+
+console.log(BoolType)
+
+
+var composition = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+
+console.log(composition.toUpperCase())
+
+ console.log(typeof composition)
+
+ 
+ var of = 25;
+
+ console.log(typeof of)
+
+
+ var sp = true;
+
+ console.log(typeof sp)
+
+ var num1 = 5
+
+ var num2 = 6
+
+ console.log(num1+num2)
+
+
+ var num3 = 6
+
+ var num4 = 9 
+
+ console.log(num3*num4)
+
+
+ var num5 = 0.0
+
+ console.log(typeof num5)
+
+ a = 5
+
+ b = 6 
+
+ c = 5+6
+
+console.log(c)
+
+// stirng
+
+
+
+ console.log(composition.toLowerCase())
+
+
+var my = "rayyan, saad, mahmud, Utsho";
+
+console.log(my.split("Utsho"));
+
+console.log(my.indexOf("U"));
+
+var a = 90 
+ var b = 52
+
+ a  = "" + a
+
+ console.log(typeof a )
+ console.log(a)
+
+
+ var mt = -10
+
+
+
+ 
+
+ var sum = Math.abs(mt)
+
+ console.log(sum)
+
+ var sum1 = Math.round(mt)
+
+ console.log(sum1)  
+
+
+
